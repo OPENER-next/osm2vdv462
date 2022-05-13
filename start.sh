711da23dbe5bddcc9a99935f2e899750806f131a
Robbendebiene <robbendebiene@mailbox.org>
2022-05-13 09:35:47 +0200

    Advance script and add osm2pgsql import

--- start.sh
+++ start.sh
@@ -1,11 +1,20 @@
+# export required for osm2pgsql
+# see https://osm2pgsql.org/doc/manual.html#environment-variables
 export PGUSER="admin"
 export PGPASSWORD="admin"
 export PGDATABASE="osm2vdv462"
+export PGPORT="5432"
+
+PG_ADMIN_EMAIL="admin@mail.com"
+PG_ADMIN_PASSWORD="admin"
+
+DOCKER_NETWORK="osm2vdv462_net"
+
 
 # Create a network to use the Postgis-Server in another container.
 # Networks are used to connect containers and allow them to communicate.
 # If it fails (for example because it already exists) ignore the error.
-docker network create osm2vdv462_net || true
+docker network create $DOCKER_NETWORK || true
 
 
 # Create docker volume which is a storage point located outside of containers.
@@ -18,31 +27,45 @@ echo "Starting postgis db docker"
 # Otherwise install and run it
 docker start osm2vdv462_postgis || docker run \
   --name "osm2vdv462_postgis" \
-  --network "osm2vdv462_net" \
-  --publish 5432:5432 \
+  --network $DOCKER_NETWORK \
+  --publish "$PGPORT:5432" \
   --volume "osm2vdv462_postgis:/var/lib/postgresql/data" \
   --env "POSTGRES_DB=$PGDATABASE" \
   --env "POSTGRES_USER=$PGUSER" \
   --env "POSTGRES_PASSWORD=$PGPASSWORD" \
-  --env "PG_PRIMARY_PORT=5432" \
+  --env "PG_PRIMARY_PORT=$PGPORT" \
   --hostname "osm2vdv462_postgis" \
   --detach \
   postgis/postgis
 
 
-read -p "Do you want to use pgadmin4? (y/n) " RESP
+read -p "Do you want to use pgadmin4? (y/n) " USE_PGADMIN4
 # Optionally install and run pgadmin for easier database management
-if [ "$RESP" = "y" ]; then
+if [ $USE_PGADMIN4 = "y" ] || [ $USE_PGADMIN4 = "Y" ]; then
   docker start pgadmin4 || docker run \
     --publish 80:80 \
     --name "pgadmin4" \
     --volume "$(pwd)/config/pgadmin_servers.json:/pgadmin4/servers.json" \
-    --env "PGADMIN_DEFAULT_EMAIL=admin@mail.com" \
-    --env "PGADMIN_DEFAULT_PASSWORD=admin" \
-    --network="osm2vdv462_net" \
+    --env "PGADMIN_DEFAULT_EMAIL=$PG_ADMIN_EMAIL" \
+    --env "PGADMIN_DEFAULT_PASSWORD=$PG_ADMIN_PASSWORD" \
+    --network=$DOCKER_NETWORK \
     --detach \
     dpage/pgadmin4
 fi
 
-read -p "Press Enter to exit"
 
+# Import osm data file
+read -p "The OSM file that should be imported (will be skipped if file cannot be found) " IMPORT_FILE
+if [ -f "$IMPORT_FILE" ]; then
+  osm2pgsql \
+    --host "localhost" \
+    --slim \
+    --drop \
+    --cache 2048 \
+    --output flex \
+    --style "$(pwd)/config/import.lua" \
+    $IMPORT_FILE
+fi
+
+
+read -p "Press Enter to exit"
