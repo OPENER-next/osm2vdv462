ddc3db95753ed54da40936e1e1c6618594340475
Janik Kaden <github@janik-kaden.de>
2023-04-06 13:15:16 +0200

    Add PPR preprocess to docker compose

--- start.sh
+++ start.sh
@@ -12,77 +12,85 @@ export DOCKER_NETWORK="osm2vdv462_net"
 
 EXPORT_FILE="export.xml"
 
+export IMPORT_FILE_PATH=""
+export IMPORT_FILE=""
+
 # Optionally import a new OSM file to be used for PPR and OSM2PGSQL
 read -p "Do you want to import an OSM file? (y/n) " RUN_IMPORT
 # Import osm data file
 if [ "$RUN_IMPORT" = "y" ] || [ "$RUN_IMPORT" = "Y" ]; then
-  read -p "Enter the OSM file(s) that should be imported: " IMPORT_FILE
-  if ! [ -f $IMPORT_FILE ]; then
+  read -p "Enter the OSM file(s) that should be imported: " IMPORT_FILE_PATH
+  if ! [ -f $IMPORT_FILE_PATH ]; then
     echo "File does not exist. Quitting ..."
     exit 1
   fi
 
+  export IMPORT_FILE_PATH
+  export IMPORT_FILE=$(basename "$IMPORT_FILE_PATH")
+
   read -p "Do you want to (re)run the routing preprocessing? (y/n) " RUN_PREPROCESSING
-  if [ "$RUN_PREPROCESSING" = "y" ] || [ "$RUN_PREPROCESSING" = "Y" ]; then
-    docker pull ghcr.io/motis-project/ppr:edge
-    docker run -u root --rm -it -v data:/data -v $IMPORT_FILE:/data/$(basename "$IMPORT_FILE") ghcr.io/motis-project/ppr:edge /ppr/ppr-preprocess --osm /data/$(basename "$IMPORT_FILE") --graph /data/germany.ppr
+  #if [ "$RUN_PREPROCESSING" = "y" ] || [ "$RUN_PREPROCESSING" = "Y" ]; then
+    #docker pull ghcr.io/motis-project/ppr:edge
+    #docker run -u root --rm -it -v data:/data -v $IMPORT_FILE:/data/$(basename "$IMPORT_FILE") ghcr.io/motis-project/ppr:edge /ppr/ppr-preprocess --osm /data/$(basename "$IMPORT_FILE") --graph /data/germany.ppr
 
     # alternatively, if [elevation data](https://github.com/motis-project/ppr/wiki/Elevation-Data-(DEM)) is used:
     # docker run -u root --rm -it -v data:/data -v $IMPORT_FILE:/data/$(basename "$IMPORT_FILE") ghcr.io/motis-project/ppr:edge -v /path/to/srtm:/srtm /ppr/ppr-preprocess --osm /data/$(basename "$IMPORT_FILE") --graph /data/germany.ppr --dem /srtm
-  fi
+  #fi
 fi
 
 # Optionally install and run pgadmin for easier database management
 read -p "Do you want to use pgadmin4? (y/n) " USE_PGADMIN4
 
-# Check if Docker Compose is already running
-if [ ! "$(docker-compose ps -q)" ]; then
-  # Start Docker Compose project:
-  echo "Starting Docker Compose project ..."
+# Start Docker Compose project:
+echo "Starting Docker Compose project ..."
 
-  if [ $USE_PGADMIN4 ]; then
-    docker-compose --profile pgadmin4 up -d
-  else
-    docker-compose up -d
+if [ "$RUN_PREPROCESSING" = "y" ] || [ "$RUN_PREPROCESSING" = "Y" ]; then
+  docker-compose up osm2vdv462_ppr_preprocess
+
+  exit_status=$(docker inspect osm2vdv462_ppr_preprocess --format='{{.State.ExitCode}}')
+
+  # always remove the preprocessing container, even if it exited succesfully
+  docker-compose rm osm2vdv462_ppr_preprocess -f
+
+  if [ ! $exit_status -eq 0 ]; then
+    echo "Error: Preprocess exited with status $exit_status. Quitting ..."
+    exit 1
   fi
-  
-  echo "Waiting for PPR to load the routing graph ..."
-  MAX_RETRIES=20
-  RETRY_DELAY=10
-  # loop until the maximum number of retries is reached
-  for i in $(seq 1 $MAX_RETRIES); do
-      # check the health of the container using curl command
-      HEALTH=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:9042)
-
-      # check the status code returned by curl
-      if [ "$HEALTH" -eq "200" ]; then
-          # exit the loop if the container is healthy
-          echo "PPR container is healthy"
-          break
-      else
-          # wait for the retry delay before checking again
-          sleep $RETRY_DELAY
-          printf "."
-      fi
-  done
-
-  # exit with error if the container is still not healthy
-  if [ "$HEALTH" -ne "200" ]; then
-      echo "Container is not healthy after $MAX_RETRIES retries"
-      exit 1
-  fi
-  echo "Docker Compose project started."
-
-else
-  # check, if PGADMIN4 is already running
-  if [ $USE_PGADMIN4 ]; then
-    docker-compose --profile pgadmin4 up -d
-    echo "Started PGADMIN4."
-  fi
-  echo "Docker Compose project is already running."
-  
 fi
 
+if [ $USE_PGADMIN4 ]; then
+  docker-compose --profile pgadmin4 up -d
+else
+  docker-compose up -d
+fi
+
+echo "Waiting for PPR to load the routing graph ..."
+MAX_RETRIES=20
+RETRY_DELAY=10
+
+# loop until the maximum number of retries is reached
+for i in $(seq 1 $MAX_RETRIES); do
+    # check the health of the container using curl command
+    HEALTH=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:9042)
+
+    # check the status code returned by curl
+    if [ "$HEALTH" -eq "200" ]; then
+        # exit the loop if the container is healthy
+        echo "PPR container is healthy"
+        break
+    else
+        # wait for the retry delay before checking again
+        sleep $RETRY_DELAY
+        printf "."
+    fi
+done
+
+# exit with error if the container is still not healthy
+if [ "$HEALTH" -ne "200" ]; then
+    echo "Container is not healthy after $MAX_RETRIES retries"
+    exit 1
+fi
+echo "Docker Compose project started."
 
 echo "Download latest public transport operator list from Wikidata:"
 csv=$(
@@ -130,7 +138,7 @@ if [ "$RUN_IMPORT" = "y" ] || [ "$RUN_IMPORT" = "Y" ]; then
     --cache 2048 \
     --output flex \
     --style "$(pwd)/scripts/osm2pgsql/main.lua" \
-    $IMPORT_FILE
+    $IMPORT_FILE_PATH
 fi
 
 
